192f3809a36974d95a6d1da7ae3875100ff123bf
Shadow Walker <franklynpeter2006@gmail.com>
2025-10-18 11:14:23 +0100

    fix: remove .amezmo deplyoment shell script

--- .amezmo/deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-if command -v composer2 >/dev/null 2>&1; then
-    composer2 install --no-ansi --no-progress --no-interaction --no-dev
-else
-    composer install --no-ansi --no-progress --no-interaction --no-dev
-fi
