bc19660439167685928bffbb7acd79dd9c12a4d5
Shadow Walker <franklynpeter2006@gmail.com>
2025-10-18 11:54:48 +0100

    refactor: move API routes to api.php routes file

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,9 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+
+require_once __DIR__ . '/api.php';
 
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/me', ProfileController::class)->name('me');
